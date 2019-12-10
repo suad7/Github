7c880594dc782609af85673de09b6bdbdeb0d2ca
Suad shire <jido@abdulazizs-MacBook-Air.local>
2019-12-10 12:40:20 +0300

    first commit

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css'],
+ 
+})
+export class GithubComponent implements OnInit {
+  
+  githubService: any;
+  username: any;
+  
+
+  constructor(private_githubService:GithubService) {
+    console.log('Github Component Init...');
+
+    this.githubService.getUser().subscribe(username =>{
+      //console.log(user);
+      this.username = this.username ;
+    });
+   }
+
+  ngOnInit() {
+  }
+
+}
